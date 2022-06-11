996c22ee65b870eb4eb0fe343da0c380170803c7
Sym <you@example.com>
2022-06-12 00:24:59 +0300

    uwu

--- Tokyo Saga.lua
+++ Tokyo Saga.lua
@@ -1,12 +1,20 @@
+-- Variables --
+
 local workspace = game:GetService("Workspace")
+local mouse = game.Players.LocalPlayer:GetMouse()
+local camera = workspace.CurrentCamera
 local gym = workspace["Local Gym"]["Local Gym"]
 local pizza = workspace.Pizza
 local bottleBuyer = workspace["Bottle Buyer"].HumanoidRootPart
 local npcBottles = workspace["Npc Bottles"].HumanoidRootPart
 
+bottleAutomation = false
+
 if pizza:IsA("Folder") then
     pizzaPlace = pizza.Pizza
 end
+
+-- Training --
 
 function Pushup()
     while true do
@@ -48,52 +56,91 @@ function Weight()
     end
 end
 
+-- Teleporter --
+
 function TeleportTo(newPlace)
     local plr = game.Players.LocalPlayer
     if plr.Character then
-        plr.Character.HumanoidRootPart.CFrame = newPlace.CFrame
+        plr.Character.HumanoidRootPart.CFrame = newPlace.CFrame * CFrame.new(0, 0, -5)
+        plr.Character.HumanoidRootPart.CFrame = plr.Character.HumanoidRootPart.CFrame * CFrame.Angles(0, math.rad(180), 0)
+        camera.CFrame = newPlace.CFrame * CFrame.Angles(math.rad(180), 0, 0)
     end
 end
 
+-- Automation --
 
--- gui --
+function bottle()
+
+    while bottleAutomation do
+        TeleportTo(npcBottles)
+        wait(1)
+        mousemoveabs(camera.ViewportSize.X / 2, (camera.ViewportSize.Y / 2) + 50)
+        mouse1click()
+        wait(2)
+        TeleportTo(bottleBuyer)
+        wait(1)
+        mousemoveabs(camera.ViewportSize.X / 2, camera.ViewportSize.Y / 2 + 50)
+        mouse1click()
+        wait(2)
+    end
+        
+end
+
+
+---- gui ----
 
 local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
+
 local w = library:CreateWindow("Tokyo Saga")
 local b = w:CreateFolder("Training")
 local c = w:CreateFolder("Teleport")
-local b2 = w:CreateFolder(".Destroy Gui")
+local d = w:CreateFolder("Automation")
+local e = w:CreateFolder(".Destroy Gui")
 
-b2:DestroyGui()
+-- Training --
 
-b:Button("Weight", function()
-    Weight()
-end)
-
-b:Button("Pushup", function()
+b:Button("Pushups",function()
     Pushup()
 end)
 
-b:Button("SitUp", function()
+b:Button("Weight",function()
+    Weight()
+end)
+
+b:Button("Situps",function()
     SitUp()
 end)
 
-b:Button("Jump Rope", function()
+b:Button("Jump Rope",function()
     JumpRope()
 end)
+
+-- Teleport -- 
 
 c:Button("Gym",function()
     TeleportTo(gym)
 end)
 
-c:Button("Pizza Shop",function()
+c:Button("Pizza Place",function()
     TeleportTo(pizzaPlace)
+end)
+
+c:Button("Bottle Seller",function()
+    TeleportTo(npcBottles)
 end)
 
 c:Button("Bottle Buyer",function()
     TeleportTo(bottleBuyer)
 end)
 
-c:Button("Npc Bottles",function()
-    TeleportTo(npcBottles)
+-- Automation --
+
+d:Toggle("Bottles",function(bool)
+    bottleAutomation = bool
+    print(bottleAutomation)
+    if bottleAutomation then bottle() end
 end)
+
+-- destroy gui --
+
+e:DestroyGui()
